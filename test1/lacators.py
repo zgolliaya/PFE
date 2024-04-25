52633e0bcc72e83fd073f7035c5e5404340ed4a0
zgolliaya <141150434+zgolliaya@users.noreply.github.com>
2024-04-25 10:31:38 +0100

    Update lacators.py

--- test1/lacators.py
+++ test1/lacators.py
@@ -3,7 +3,7 @@ usernamexpath="xpath=//input[contains(@formcontrolname,'username')] "
 passwordxpath="xpath=//input[contains(@formcontrolname,'password')]"
 continuebutton="xpath=//button[contains(@class,'btn btn-primary')] "
 #ADD operator
-operatorsbutton1="xpath=//span[text()='Operators']"
+#operatorsbutton1="xpath=//span[text()='Operators']"
 addoperators="xpath=//button[contains(@routerlink,'user')]"
 firstnameop="xpath=//input[@formcontrolname='firstname'] "
 lastnameop="xpath=//input[@formcontrolname='lastname'] "
